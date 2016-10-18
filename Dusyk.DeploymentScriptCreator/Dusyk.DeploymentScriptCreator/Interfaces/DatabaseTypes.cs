e82837c3512586d60664bfee0d6bde5ca002895e
Chris Dusyk <chris.dusyk@gmail.com>
2016-10-18 11:25:21 -0600

    Added PackageCreator interface and started Oracle implementation

---
+++ Dusyk.DeploymentScriptCreator/Dusyk.DeploymentScriptCreator/Interfaces/DatabaseTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dusyk.DeploymentScriptCreator
+{
+	internal enum DatabaseTypes
+	{
+		Oracle = 0,
+		SqlServer = 1,
+		MySQL = 2,
+		PostgreSQL = 3
+	}
+}
---
+++ Dusyk.DeploymentScriptCreator/Dusyk.DeploymentScriptCreator/Interfaces/IPackageCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dusyk.DeploymentScriptCreator.Interfaces
+{
+	interface IPackageCreator
+	{
+		string OutputPath { get; set; }
+
+		string OutputFileName { get; set; }
+
+		List<string> Files { get; set; }
+
+		bool CreateScript();
+	}
+}
