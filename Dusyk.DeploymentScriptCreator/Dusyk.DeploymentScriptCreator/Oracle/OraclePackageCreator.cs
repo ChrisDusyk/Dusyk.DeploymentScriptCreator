c22092bd3e3f6b60cf945b9f000f73d95be5338c
Chris Dusyk <chris.dusyk@gmail.com>
2016-10-18 12:19:25 -0600

    Completed base file append and output

--- Dusyk.DeploymentScriptCreator/Dusyk.DeploymentScriptCreator/Oracle/OraclePackageCreator.cs
+++ Dusyk.DeploymentScriptCreator/Dusyk.DeploymentScriptCreator/Oracle/OraclePackageCreator.cs
@@ -1,6 +1,8 @@
 ﻿using Dusyk.DeploymentScriptCreator.Interfaces;
+using NLog;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,15 +17,36 @@ namespace Dusyk.DeploymentScriptCreator.Oracle
 
 		public string OutputFileName { get; set; }
 
+		private static Logger _logger = LogManager.GetCurrentClassLogger();
+
 		public bool CreateScript()
 		{
 			try
 			{
+				using (FileStream outputWriter = new FileStream($"{OutputPath}\\{OutputFileName}", FileMode.Create))
+				{
+					foreach (string fileName in Files)
+					{
+						// Read all text from the file. File.ReadAllText closes the file when completed.
+						string fileContents = File.ReadAllText(fileName);
 
+						StringBuilder outputBuilder = new StringBuilder();
+						outputBuilder.Append(fileContents);
+						outputBuilder.AppendLine();
+						outputBuilder.AppendLine("COMMIT;");
+						outputBuilder.AppendLine();
+
+						byte[] outputBytes = Encoding.UTF8.GetBytes(outputBuilder.ToString());
+
+						outputWriter.Write(outputBytes, 0, outputBytes.Length);
+
+						_logger.Info("File: {0} added to script.", fileName);
+					}
+				}
 			}
 			catch (Exception ex)
 			{
-				// log ex
+				_logger.Error(ex, ex.Message);
 				return false;
 			}
 
