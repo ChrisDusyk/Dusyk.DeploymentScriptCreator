46cacbc949c395d239cb31ab928f2ecfa6fb9d3b
Chris Dusyk <chris.dusyk@gmail.com>
2016-10-18 15:09:18 -0600

    Added output folder location picker

--- Dusyk.DeploymentScriptCreator/Dusyk.DeploymentScriptCreator/MainWindow.xaml.cs
+++ Dusyk.DeploymentScriptCreator/Dusyk.DeploymentScriptCreator/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace Dusyk.DeploymentScriptCreator
 {
@@ -20,9 +22,39 @@ namespace Dusyk.DeploymentScriptCreator
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private string _outputFolder;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void OutputFolderDialogSelector_Click(object sender, RoutedEventArgs e)
+		{
+			var folderDialog = new CommonOpenFileDialog();
+
+			folderDialog.AllowNonFileSystemItems = true;
+			folderDialog.IsFolderPicker = true;
+			folderDialog.Title = "Select script output folder";
+
+			var result = folderDialog.ShowDialog();
+
+			switch (result)
+			{
+				case CommonFileDialogResult.Ok:
+					var folder = folderDialog.FileName;
+					OutputFolderTextbox.Text = folder;
+					OutputFolderTextbox.ToolTip = folder;
+
+					_outputFolder = folderDialog.FileName;
+
+					break;
+				case CommonFileDialogResult.Cancel:
+				default:
+					OutputFolderTextbox.Text = null;
+					OutputFolderTextbox.ToolTip = null;
+					break;
+			}
 		}
 	}
 }
